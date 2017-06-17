795085933a55a5be0aea0634a5bd0504c4ef3895
kiluhabe <vvasravv@gmail.com>
2017-06-17 23:24:56 +0900

    modify message

--- fish/config.fish
+++ fish/config.fish
@@ -9,14 +9,14 @@ set fish_plugins theme peco
 if test -d $HOME/.local/share/omf
   set -gx OMF_PATH "$HOME/.local/share/omf"
 else
-  echo "Oh My Fish was not found."
+  NOT_FOUNDS $NOT_FOUNDS "Oh My Fish"
 end
 
 # Customize Oh My Fish configuration path.
 if test -d $HOME/.config/omf
   set -gx OMF_CONFIG "$HOME/.config/omf"
 else
-  echo "Oh My Fish Configration File was not found."
+  NOT_FOUNDS $NOT_FOUNDS "Oh My Fish Configration File"
 end
 
 # Load oh-my-fish configuration.
@@ -34,7 +34,7 @@ if test -d $HOME/.rbenv
   set -gx fish_user_paths $fish_user_paths $RBENV_ROOT/bin $RBENV_ROOT/shims
   rbenv rehash >/dev/null ^&1
 else
-  echo "rbenv was not found."
+  NOT_FOUNDS $NOT_FOUNDS "rbenv"
 end
 
 # pyenv setup
@@ -44,7 +44,7 @@ if test -d $HOME/.pyenv
   set -gx fish_user_paths $fish_user_paths $PYENV_ROOT/bin $PYENV_ROOT/shims
   pyenv rehash >/dev/null ^&1
 else
-  echo "pyenv was not found."
+  NOT_FOUNDS $NOT_FOUNDS "pyenv"
 end
 
 #node
@@ -54,21 +54,21 @@ if test -d $HOME/.nodenv
   set -gx fish_user_paths  $fish_user_paths $NODENV_ROOT/bin $NODENV_ROOT/shims
   nodenv rehash >/dev/null ^&1
 else
-  echo "nodenv was not found."
+  NOT_FOUNDS $NOT_FOUNDS "nodenv"
 end
 
 #tmux plugin path
 if test -d $HOME/.tmux/plugins/
   set -gx TMUX_PLUGIN_PATH $HOME/.tmux/plugins/
 else
-  echo "tmux plugings directory was not found."
+  NOT_FOUNDS $NOT_FOUNDS "tmux plugings directory"
 end
 
 # The next line enables shell command completion for gcloud.
 if test -d $HOME/google-cloud-sdk/bin
   set -gx fish_user_paths $fish_user_paths $HOME/google-cloud-sdk/bin
 else
-  echo "gcloud sdk was not found."
+  NOT_FOUNDS $NOT_FOUNDS "gcloud sdk"
 end
 
 #gopath
@@ -76,7 +76,7 @@ if test -d $HOME/gocode
   set -gx GOPATH $HOME/gocode
   set -gx fish_user_paths $fish_user_paths $GOPATH/bin
 else
-  echo "gocode directory was not found."
+  NOT_FOUNDS $NOT_FOUNDS "gocode directory"
 end
 
 #rust path
@@ -86,7 +86,7 @@ if test -d $HOME/.cargo; and test -d $HOME/.rustup
   set -gx RUST_SRC_PATH $HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
   set -gx fish_user_paths $fish_user_paths $CARGO_HOME/bin
 else
-  echo "cargo or rustup was not found."
+  NOT_FOUNDS $NOT_FOUNDS "cargo or rustup"
 end
 
 #alias
@@ -102,4 +102,9 @@ alias vi='vim'
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
   bind \c] peco_change_directory # Bind for prco change directory to Ctrl+]
+end
+
+if test -z '$NOT_FOUNDS'
+  echo 'these are not found.'
+  echo $NOT_FOUNDS
 end
