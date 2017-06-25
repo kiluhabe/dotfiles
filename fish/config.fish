396a5a38419f7d42c3f2a7823cc38364613cf715
kiluhabe <kiluhabe@users.noreply.github.com>
2017-06-26 02:51:00 +0900

    fix dotcheck

--- fish/config.fish
+++ fish/config.fish
@@ -90,7 +90,7 @@ else
 end
 
 #alias
-alias dotupdate='git -C $HOME/.dotfiles pull'
+alias dotupdate='git -C $HOME/.dotfiles pull origin master'
 alias tpm-init='git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'
 alias docker-clean='docker rmi (docker images -aqf "dangling=true") 2> /dev/null; docker rm (docker ps -aqf "status=exited") 2> /dev/null'
 alias refish='exec fish -l'
