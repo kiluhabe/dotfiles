bfe96ead33f6c578ec83eb1e3612460ff697458e
kiluhabe <kiluhabe@users.noreply.github.com>
2017-07-21 17:20:19 +0900

    not_foundを表示しない

--- fish/config.fish
+++ fish/config.fish
@@ -8,15 +8,11 @@ set fish_plugins theme peco
 # Path to Oh My Fish install.
 if test -d $HOME/.local/share/omf
   set -gx OMF_PATH "$HOME/.local/share/omf"
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "Oh My Fish"
 end
 
 # Customize Oh My Fish configuration path.
 if test -d $HOME/.config/omf
   set -gx OMF_CONFIG "$HOME/.config/omf"
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "Oh My Fish Configration File"
 end
 
 # Load oh-my-fish configuration.
@@ -30,8 +26,6 @@ if test -d $HOME/.rbenv
   set -gx RBENV_ROOT $HOME/.rbenv
   set -gx fish_user_paths $fish_user_paths $RBENV_ROOT/bin $RBENV_ROOT/shims
   rbenv rehash >/dev/null ^&1
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "rbenv"
 end
 
 # pyenv setup
@@ -40,8 +34,6 @@ if test -d $HOME/.pyenv
   set -gx PYENV_ROOT $HOME/.pyenv
   set -gx fish_user_paths $fish_user_paths $PYENV_ROOT/bin $PYENV_ROOT/shims
   pyenv rehash >/dev/null ^&1
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "pyenv"
 end
 
 #node
@@ -50,30 +42,22 @@ if test -d $HOME/.nodenv
   set -gx NODENV_ROOT $HOME/.nodenv
   set -gx fish_user_paths  $fish_user_paths $NODENV_ROOT/bin $NODENV_ROOT/shims
   nodenv rehash >/dev/null ^&1
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "nodenv"
 end
 
 #tmux plugin path
 if test -d $HOME/.tmux/plugins/
   set -gx TMUX_PLUGIN_PATH $HOME/.tmux/plugins/
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "tmux plugings directory"
 end
 
 # The next line enables shell command completion for gcloud.
 if test -d $HOME/google-cloud-sdk/bin
   set -gx fish_user_paths $fish_user_paths $HOME/google-cloud-sdk/bin
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "gcloud sdk"
 end
 
 #gopath
 if test -d $HOME/gocode
   set -gx GOPATH $HOME/gocode
   set -gx fish_user_paths $fish_user_paths $GOPATH/bin
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "gocode directory"
 end
 
 #rust path
@@ -82,8 +66,6 @@ if test -d $HOME/.cargo; and test -d $HOME/.rustup
   set -gx RUSTUP_HOME $HOME/.rustup
   set -gx RUST_SRC_PATH $HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
   set -gx fish_user_paths $fish_user_paths $CARGO_HOME/bin
-else
-  set -gx NOT_FOUNDS $NOT_FOUNDS "cargo or rustup"
 end
 
 # homebrew
@@ -120,11 +102,6 @@ end
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
   bind \c] peco_change_directory # Bind for prco change directory to Ctrl+]
-end
-
-if test -n "$NOT_FOUNDS"
-  echo 'these are not found.'
-  echo $NOT_FOUNDS
 end
 
 set dotcheck_source (functions dotcheck)
