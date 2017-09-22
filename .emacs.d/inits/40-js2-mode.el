840a7f5f859b1f6635d5edfdbfa59e68ff104b2f
kiluhabe <kiluhabe@users.noreply.github.com>
2017-09-23 00:26:56 +0900

    fix js2 mode

--- .emacs.d/inits/40-js2-mode.el
+++ .emacs.d/inits/40-js2-mode.el
@@ -1,3 +1,18 @@
 (require 'js2-mode)
+(require 'flycheck)
+(require 'company)
+
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+
+(defun setup-js2-mode ()
+  (interactive)
+  (setq js2-basic-offset 2)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(idle-change mode-enabled new-line save))
+  (set-face-attribute 'flycheck-error nil :background "DarkRed")
+  (company-mode +1)
+  (setq company-idle-delay 0.1)
+  (setq company-minimum-prefix-length 1)
+)
+
+(add-hook 'js2-mode-hook #'setup-js2-mode)
