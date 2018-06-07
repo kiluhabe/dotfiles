6bf8130431a167cffa96fb2587e92d008c59c6d3
kiluhabe <kiluhabe@users.noreply.github.com>
2018-06-08 07:56:12 +0900

    :art: config treemacs

--- .emacs.d/inits/30-treemacs.el
+++ .emacs.d/inits/30-treemacs.el
@@ -3,3 +3,12 @@
 (treemacs-filewatch-mode t)
 (setq treemacs-width 30)
 (setq treemacs-file-event-delay 500)
+(setq treemacs-collapse-dirs 3)
+
+;; (treemacs-git-mode 'simple)
+(treemacs-follow-mode t)
+(treemacs-filewatch-mode t)
+
+(set-face-foreground 'treemacs-directory-face "#dcdcdc")
+
+(global-set-key [f8] 'treemacs)
