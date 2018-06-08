707e3e254efc7e4b66553e695004b172bc02213a
kiluhabe <kiluhabe@users.noreply.github.com>
2018-06-09 05:51:15 +0900

    :lipstick: beautify gutter

--- .emacs.d/inits/30-git-gutter.el
+++ .emacs.d/inits/30-git-gutter.el
@@ -4,9 +4,9 @@
 
 (custom-set-variables
  '(git-gutter:window-width 2)
- '(git-gutter:modified-sign "・")
- '(git-gutter:added-sign "・")
- '(git-gutter:deleted-sign "・"))
+ '(git-gutter:modified-sign "✹")
+ '(git-gutter:added-sign "✹")
+ '(git-gutter:deleted-sign "✹"))
 
 (set-face-foreground 'git-gutter:modified "#ff8c00")
 (set-face-foreground 'git-gutter:added "#698b69")
