131f3e5184135c15dd854463c9d2dcf4f793ca69
kiluhabe <kiluhabe@users.noreply.github.com>
2018-07-13 07:08:26 +0900

    fix

--- scripts/mopidy/install.sh
+++ scripts/mopidy/install.sh
@@ -2,5 +2,5 @@
 
 brew tap mopidy/mopidy
 brew install mopidy
-brew reinstall gst-python --without-python --with-python@2
 brew install mopidy-spotify ncmpcpp
+brew reinstall gst-python --without-python --with-python@2
