a2a8844c8207f84c86703d73073d71af1a687f01
kiluhabe <kiluhabe@users.noreply.github.com>
2018-07-13 06:22:36 +0900

    fix

--- scripts/pyenv/install.sh
+++ scripts/pyenv/install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-if [ ! -z $(which pyenv) ]; then
-    echo "pyenv installed."
-    return 0
-fi
+main() {
+    if [ ! -z $(which pyenv) ]; then
+        echo "pyenv installed."
+        return 0
+    fi
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-cd pyenv/plugins/python-build && ./install.sh
-cd -
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    cd pyenv/plugins/python-build && ./install.sh
+    cd $HOME
+}
+
+main
