7c01ea2ac1fdc974cd52f35879fe3168c0fe661a
kiluhabe <kiluhabe@users.noreply.github.com>
2018-10-02 18:01:18 +0900

    :sparkles: add taranslate-shell

--- scripts/brew/install.sh
+++ scripts/brew/install.sh
@@ -3,7 +3,7 @@ if [ -d $(which brew 2>/dev/null) ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install cask docker-compose neofetch imagemagick fzf tmux emacs direnv yarn bash
+brew install cask docker-compose neofetch imagemagick fzf tmux emacs direnv yarn bash translate-shell
 brew cask install firefox google-chrome iterm2 docker visual-studio-code google-japanese-ime notion pd dropbox postman clipy
 
 brew tap httpu/httpu
