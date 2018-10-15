2822ef76d1de5e464e58360673b11b75c8df70b0
kiluhabe <kiluhabe@users.noreply.github.com>
2018-10-15 10:29:58 +0900

    :recycle: fix fixssh command

--- commands/fixssh.sh
+++ commands/fixssh.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 fixssh() {
-  if [ -z {$TMUX+x} ]; then
-    export SSH_AUTH_SOCK=$(tmux show-environment | grep '^SSH_AUTH_SOCK' | cut -d'=' -f2)
-    echo "SSH_AUTH_SOCK has been fixed."
-    return 0
-  fi
-
-  echo "you are not running on tmux."
-  return 1
+  export SSH_AUTH_SOCK=$(tmux show-environment | grep '^SSH_AUTH_SOCK' | cut -d'=' -f2)
+  echo "SSH_AUTH_SOCK has been fixed."
+  return 0
 }
