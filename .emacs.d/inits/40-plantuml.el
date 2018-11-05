8c036c9bdbbfb2ebdd6221b8725d9edfe9d47e52
kiluhabe <kiluhabe@users.noreply.github.com>
2018-11-05 11:14:17 +0900

    add plantuml config

--- .emacs.d/inits/40-plantuml.el
+++ .emacs.d/inits/40-plantuml.el
@@ -2,3 +2,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
 (add-to-list 'auto-mode-alist '("\\.pu\\'" . plantuml-mode))
+
+(setq plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
+(setq plantuml-output-type "svg")
+(setq plantuml-options "-charset UTF-8")
