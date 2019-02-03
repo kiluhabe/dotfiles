06fb50209532bc8992d61f6056aad8a525a0c2d4
kiluhabe <kiluhabe@users.noreply.github.com>
2019-02-04 04:49:46 +0900

    add fff keybind

--- emacs/.emacs.d/inits/30-company.el
+++ emacs/.emacs.d/inits/30-company.el
@@ -7,6 +7,7 @@
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 2)
 (setq company-selection-wrap-around t)
+(setq company-dabbrev-downcase nil)
 
 (set-face-attribute 'company-tooltip nil
                     :foreground "gainsboro" :background "#262626")
