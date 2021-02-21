c7d00fe0cd27ee0ec42c581069e71a7e9119e96c
kiluhabe <kiluhabe@users.noreply.github.com>
2021-02-21 18:28:35 +0900

    update

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar left &
-polybar right &
+polybar one &
+polybar two &
+polybar three &
 
 echo "Bars launched..."
