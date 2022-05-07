8f2241c9fe52e06a08c0156a628ca438bbdd73ea
kiluhabe <kiluhabe@users.noreply.github.com>
2022-05-08 04:19:47 +0900

    update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,6 +3,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(setq gc-cons-threshold 100000000)
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
   (add-to-list 'load-path default-directory)
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
@@ -15,24 +16,5 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (setq custom-theme-directory "~/.emacs.d/themes")
 (load-theme 'twilight t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(git-gutter:added-sign "・")
- '(git-gutter:deleted-sign "・")
- '(git-gutter:modified-sign "・")
- '(git-gutter:window-width 2)
- '(package-selected-packages '(tabbar))
- '(ruby-insert-encoding-magic-comment nil)
- '(tabbar-separator '(0.5)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
