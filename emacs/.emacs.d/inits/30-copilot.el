9dca30abbe140143f44d244f3e26ab76e8393bfc
kiluhabe <kiluhabe@users.noreply.github.com>
2023-07-11 11:05:27 +0900

    update

---
+++ emacs/.emacs.d/inits/30-copilot.el
@@ -0,0 +1,9 @@
+(use-package copilot
+  :quelpa (copilot :fetcher github
+                   :repo "zerolfx/copilot.el"
+                   :branch "main"
+                   :files ("dist" "*.el"))
+  :config
+  (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+  (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
+;; you can utilize :map :hook and :config to customize copilot
