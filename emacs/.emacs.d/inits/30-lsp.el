a4497ac2cae1371bfa62f88463d9d903d0513bb3
kiluhabe <kiluhabe@users.noreply.github.com>
2024-05-21 16:36:43 +0900

    fix typescript-mode

--- emacs/.emacs.d/inits/30-lsp.el
+++ emacs/.emacs.d/inits/30-lsp.el
@@ -1,13 +1,8 @@
 (use-package lsp-mode
   :ensure t
   :defer t
-  :init
   (setq lsp-keymap-prefix "C-c l")
-  :after
-  (lsp lsp-deferred)
-  :config
-  (lsp-enable-which-key-integration)
-  (eldoc-mode +1))
+  :commands (lsp lsp-deferred))
 
 (use-package lsp-ui
   :ensure t
--- emacs/.emacs.d/inits/30-multiple-cursors.el
+++ emacs/.emacs.d/inits/30-multiple-cursors.el
@@ -1,7 +1,7 @@
 (use-package multiple-cursors
-             :config
-             (multiple-cursors-mode t)
-             :bind (
-             ("C-c <down>" . mc/mark-next-like-this)
-             ("C-c <up>" . mc/mark-previous-like-this)
-             ("C-c C-a" . mc/mark-all-like-this)))
+  :config
+  (multiple-cursors-mode t)
+  :bind (
+         ("C-c <down>" . mc/mark-next-like-this)
+         ("C-c <up>" . mc/mark-previous-like-this)
+         ("C-c C-a" . mc/mark-all-like-this)))
