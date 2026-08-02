15e6f9d6b4cfeb0ee0a969f3899686875953fbe0
kiluhabe <kiluhabe@users.noreply.github.com>
2026-08-02 15:43:48 +0900

    Move Telescope find_files to Cmd+P, add project search and command palette

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,9 @@ return {
   branch = "0.1.x",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<D-p>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+    { "<D-S-f>", "<cmd>Telescope live_grep<cr>", desc = "Live grep (project)" },
+    { "<D-S-p>", "<cmd>Telescope commands<cr>", desc = "Command palette" },
   },
 }
